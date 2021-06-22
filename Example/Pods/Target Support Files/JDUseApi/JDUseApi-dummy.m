daaa8907082b24bf90481ec486d5264268bfa01c
jdetrizep <76084420+jdetrizep@users.noreply.github.com>
2021-06-21 20:36:23 -0600

    Initail commit with cocoapod lib tamplate

---
+++ Example/Pods/Target Support Files/JDUseApi/JDUseApi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JDUseApi : NSObject
+@end
+@implementation PodsDummy_JDUseApi
+@end
---
+++ JDUseApi/Classes/ApiError.swift
@@ -0,0 +1,18 @@
+//
+//  ApiError.swift
+//  MiForo
+//
+//  Created by Jorge De Trinidad Zepeda on 19/6/21.
+//
+
+import Foundation
+
+enum ApiError: Error {
+    case requestFailed(description: String)
+    case jsonConversionFailure(description: String)
+    case invalidData
+    case responseUnsuccessful(description: String)
+    case jsonParsingFailure
+    case noInternet
+    case failedSerialization
+}
